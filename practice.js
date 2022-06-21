0297e96df611a249b0cb98beb364adc4d2d03ad0
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-21 10:40:39 -0400

    Adding stuff

--- practice.js
+++ practice.js
@@ -1,11 +1,9 @@
-const capitalize = string => {
-    if(typeof string === "string"){
+const capitalize = (string) => {
+  if (typeof string === "string") {
+    return (string = string.slice(0, 1).toUpperCase() + string.slice(1));
+  }
 
-        return string = string.slice(0, 1).toUpperCase() + string.slice(1);
-    }
-    else {
-        return "ERROR"
-    }
-}
+  return "ERROR";
+};
 
 module.exports = capitalize;
--- practice.test.js
+++ practice.test.js
@@ -1,18 +1,20 @@
-const capitalize = require('./practice')
+const capitalize = require("./practice");
 
-describe('capitalize', () => {
-    test('works with a non-capitalized string', () => {
-        expect(capitalize("enigma")).toBe("Enigma")
-    });
-    test('works with an already capitalized string', () => {
-        expect(capitalize("Enigma")).toBe("Enigma")
-    });
-    test('fails with a number', () => {
-        expect(capitalize(1320)).toBe('ERROR')
-    });
-    test('fails with an object', () => {
-        expect(capitalize({
-            "age": 18,
-        })).toBe('ERROR')
-    });
-})
+describe("capitalize", () => {
+  test("works with a non-capitalized string", () => {
+    expect(capitalize("enigma")).toBe("Enigma");
+  });
+  test("works with an already capitalized string", () => {
+    expect(capitalize("Enigma")).toBe("Enigma");
+  });
+  test("fails with a number", () => {
+    expect(capitalize(1320)).toBe("ERROR");
+  });
+  test("fails with an object", () => {
+    expect(
+      capitalize({
+        age: 18,
+      })
+    ).toBe("ERROR");
+  });
+});
