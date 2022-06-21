c1d6b31188dbeacc620a934b88d4966b20f480d4
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-21 10:25:25 -0400

    Writing more tests for the 'capitalize' function

--- practice.test.js
+++ practice.test.js
@@ -1,5 +1,18 @@
+const capitalize = require('./practice')
+
 describe('capitalize', () => {
-    test('works with a non-capitalized number', () => {
+    test('works with a non-capitalized string', () => {
         expect(capitalize("enigma")).toBe("Enigma")
+    });
+    test('works with an already capitalized string', () => {
+        expect(capitalize("Enigma")).toBe("Enigma")
+    });
+    test('fails with a number', () => {
+        expect(capitalize(1320)).toBe("Enigma")
+    });
+    test('fails with an object', () => {
+        expect(capitalize({
+            "age": 18,
+        })).toBe("Enigma")
     });
 })
