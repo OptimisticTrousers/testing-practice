6f9c52b213c3d61db4e4dc837fd83ff0684ca37e
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-21 17:58:39 -0400

    Adding more tests

---
+++ analyzeArray/analyzeArray.test.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+const analyzeArray = require("./analyzeArray");
+
+describe("analyzing array", () => {
+  test("even array", () => {
+    expect(analyzeArray([3, 4, 1, 2])).toEqual({
+      average: 2.5,
+      min: 1,
+      max: 4,
+      length: 4,
+    });
+  });
+  test("odd array", () => {
+    expect(analyzeArray([12, 8, 4])).toEqual({
+      average: 8,
+      min: 4,
+      max: 12,
+      length: 3,
+    });
+  });
+  test("non-integer average", () => {
+    expect(analyzeArray([3, 4, 4])).toEqual({
+      average: 3.66,
+      min: 4,
+      max: 12,
+      length: 3,
+    });
+  });
+  test("object array element", () => {
+    expect(analyzeArray([{"name": "bob"}, 4, 4])).toEqual({
+      average: 3.66,
+      min: 4,
+      max: 12,
+      length: 3,
+    });
+  });
+  test("string array element", () => {
+    expect(analyzeArray(["4", 4, 4])).toEqual({
+      average: 4,
+      min: 4,
+      max: 12,
+      length: 3,
+    });
+  });
+});
